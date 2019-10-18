385e62fdd95eff40ce4b89d5930f333e328884ee
Michael Craddock <michael@moblimic.com>
2019-10-18 15:37:16 +0100

    getting to feature parity

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ function App() {
       <header className="App-header">
         <h1>Do the thing</h1>
       </header>
-      <MainView />
-      <small class="cp">Brought to you by <a href="https://moblimic.com">MobliMic</a> - Background CC <a
+      <main className='container'>
+        <MainView />
+      </main>
+      <small className="cp">Brought to you by <a href="https://moblimic.com">MobliMic</a> - Background CC <a
         href="https://www.flickr.com/photos/cretatus/">Richard Williams</a> - <a
         href="https://github.com/MobliMic/MobliTab">Github project</a></small>
     </div>
--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export const MainView = () => {
+  useEffect(() => {
+    //eslint-disable-next-line
+    chrome.bookmarks.getTree(function (bookmarks) {
+      var bookmarkList = document.getElementById('bookmarkList');
+      var list = document.createDocumentFragment();
+
+      bookmarks[0].children[0].children.forEach(function (val) {
+        var li = document.createElement('li');
+        li.className = 'linkListItem';
+        var link = document.createElement('a');
+        link.href = val.url;
+        link.textContent = val.title;
+        li.appendChild(link);
+
+        list.insertBefore(li, list.firstChild);
+      });
+
+      bookmarkList.appendChild(list);
+    });
+
+    //eslint-disable-next-line
+    chrome.topSites.get(function (sites) {
+      var recentList = document.getElementById('recentList');
+      var list = document.createDocumentFragment();
+
+      sites.forEach(function (val) {
+        var li = document.createElement('li');
+        li.className = 'linkListItem';
+        var link = document.createElement('a');
+        link.href = val.url;
+        link.textContent = val.title;
+
+        //var img = document.createElement('img');
+        //img.src = 'http:/placehold.it/50/50';
+
+        //link.appendChild(img);
+
+        li.appendChild(link);
+
+        list.appendChild(li);
+      });
+
+      recentList.appendChild(list);
+    });
+
+  });
+
   return (
     <React.Fragment>
       <section class="block" id="favorites">
